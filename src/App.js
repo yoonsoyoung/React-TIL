702a5757318247b516e63969763c2e0adb51fad5
yoonsoyoung <hummingsy@gmail.com>
2021-12-13 21:38:29 +0900

    Feat App.js : Update using state & dynamic event

--- src/App.js
+++ src/App.js
@@ -8,15 +8,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'welcome',
+      welcome: { title: 'Welcome', desc: 'Hello React!!!' },
       subject: { title: 'WEB', sub: 'World Wide Web!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design.' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive.' },
+      ],
     };
   }
   render() {
+    console.log('App render');
+    var _title,
+      _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className='App'>
         <Subject title={this.state.subject.title} sub={this.state.subject.sub} />
-        <TOC />
-        <Content title='HTML' desc='HTML is HyperText Markup Language.' />
+        <TOC data={this.state.contents} />
+        <Content title={_title} desc={_desc} />
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 class TOC extends Component {
   render() {
+    console.log('TOC render');
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a href={'/content/' + data[i].id}>{data[i].title}</a>
+        </li>
+      );
+      i = i + 1;
+    }
     return (
       <nav>
-        <ul>
-          <li>
-            <a href='1.html'>HTML</a>
-          </li>
-          <li>
-            <a href='2.html'>CSS</a>
-          </li>
-          <li>
-            <a href='3.html'>JavaScript</a>
-          </li>
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     );
   }
