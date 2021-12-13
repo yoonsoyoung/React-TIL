80bcd09865584584cc1297f01a56e49aebf28aaa
yoonsoyoung <hummingsy@gmail.com>
2021-12-13 20:48:26 +0900

    Feat App.js : Add 컴포넌트 + Refactor props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Subject />
+        <Subject title='WEB' sub='world wide web!' />
+        <Subject title='React' sub='For UI' />
+        <TOC />
+        <Content title='HTML' desc='HTML is HyperText Markup Language.' />
       </div>
     );
   }
@@ -15,9 +18,40 @@ class Subject extends Component {
   render() {
     return (
       <header>
-        <h1>WEB</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href='1.html'>HTML</a>
+          </li>
+          <li>
+            <a href='2.html'>CSS</a>
+          </li>
+          <li>
+            <a href='3.html'>JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
     );
   }
 }
