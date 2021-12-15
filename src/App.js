442570a707c7d7c404c4ede3d2129ef2af0eeb89
yoonsoyoung <hummingsy@gmail.com>
2021-12-15 23:22:12 +0900

    Feat App.js : Update event 속성을 통한 값 추출

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode: 'read',
+      selected_content_id: 2,
       welcome: { title: 'Welcome', desc: 'Hello React!!!' },
       subject: { title: 'WEB', sub: 'World Wide Web!' },
       contents: [
@@ -26,8 +27,16 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     // console.log('render', this);
     return (
@@ -39,7 +48,12 @@ class App extends Component {
             this.setState({ mode: 'welcome' });
           }.bind(this)}
         />
-        <TOC data={this.state.contents} />
+        <TOC
+          data={this.state.contents}
+          onChangePage={function (id) {
+            this.setState({ mode: 'read', selected_content_id: Number(id) });
+          }.bind(this)}
+        />
         <Content title={_title} desc={_desc} />
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,7 +8,16 @@ class TOC extends Component {
     while (i < data.length) {
       lists.push(
         <li key={data[i].id}>
-          <a href={'/content/' + data[i].id}>{data[i].title}</a>
+          <a
+            href={'/content/' + data[i].id}
+            data-id={data[i].id}
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+          >
+            {data[i].title}
+          </a>
         </li>
       );
       i = i + 1;
