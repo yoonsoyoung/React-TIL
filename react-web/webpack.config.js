da59b0e37e802e79cd852df9c979b4033e9142b6
yoonsoyoung <hummingsy@gmail.com>
2021-12-22 22:33:51 +0900

    Feat web-app : Add HTML & JS

--- react-web/webpack.config.js
+++ react-web/webpack.config.js
@@ -1,8 +1,9 @@
 // webpack 을 불러옴. ES6 부터는 import 사용
-// var webpack = require('webpack');
-import webpack from 'webpack';
+var webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
+
   // entry 파일부터 시작해서 require 불러옴
   entry: './src/index.js',
 
@@ -14,21 +15,21 @@ module.exports = {
   // 개발 서버 설정
   devServer: {
     hot: true,
-    inline: true,
+    //inline: true,
     host: '0.0.0.0',
     port: 4000,
-    contentBase: __dirname + '/public/',
+    static: __dirname + '/public/',
   },
 
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/,
-        query: {
+        options: {
           cacheDirectory: true,
-          presets: ['latest', 'react'],
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
     ],
