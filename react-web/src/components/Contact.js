9afcce061b115987753072f7ce4648e6f0e74bff
yoonsoyoung <hummingsy@gmail.com>
2022-01-05 00:29:09 +0900

    Feat Contact : Update 검색기능 use filter function

--- react-web/src/components/Contact.js
+++ react-web/src/components/Contact.js
@@ -5,6 +5,7 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      keyword: '',
       contactData: [
         {
           name: 'Gildong',
@@ -28,10 +29,23 @@ export default class Contact extends Component {
         },
       ],
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange(e) {
+    this.setState({
+      keyword: e.target.value,
+    });
+  }
   render() {
     const mapToComponents = (data) => {
+      data.sort();
+      // 검색 기능
+      data = data.filter((contact) => {
+        // 해당 키워드가 있는 애들만 보이게
+        return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+      });
       return data.map((contact, i) => {
         return <ContactInfo contact={contact} key={i} />;
       });
@@ -40,6 +54,7 @@ export default class Contact extends Component {
     return (
       <div>
         <h1>Contacts</h1>
+        <input name='keyword' placeholder='Search' onChange={this.handleChange}></input>
         <div>{mapToComponents(this.state.contactData)}</div>
       </div>
     );
