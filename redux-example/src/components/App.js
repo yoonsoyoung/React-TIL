84c46af0a0beb66c35deca7b6dad3d66a4cb51f1
yoonsoyoung <hummingsy@gmail.com>
2022-07-14 13:21:36 +0900

    Feat redux-example : Update 리덕스 연습 프로젝트 세팅

---
+++ redux-example/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        App
+        <h1>여긴 앱이다.</h1>
+
+      </div>
+    )
+  }
+}
