bf2a0a4e2d8f119f6184904531726db0168f5a5d
yoonsoyoung <hummingsy@gmail.com>
2022-07-27 10:21:21 +0900

    Feat express-tutorial : Update 미들웨어 작성

--- express-tutorial/main.js
+++ express-tutorial/main.js
@@ -2,6 +2,14 @@ var express = require('express'); // express를 불러옴. =import
 var app = express();
 var user = require('./routes/user'); // 유저 라우트 모듈 불러옴
 
+// 미들웨어 작성
+var myLogger = function(req, res, next) {
+	console.log(req.url);
+	next();
+};
+
+app.use(myLogger);
+
 app.use('/user', user);
 
 app.get('/', function(req, res){
