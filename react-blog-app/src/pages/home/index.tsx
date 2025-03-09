1c558c60f91ed01b967ac7fb992c5586a8d3306d
yoonsoyoung <hummingsy@gmail.com>
2025-03-09 23:54:01 +0900

    Feat feature/2-3: Update 글 리스트 컴포넌트화+메인,프로필,게시글 메뉴 내 리스트 컴포넌트 추가+css 적용

--- react-blog-app/src/pages/home/index.tsx
+++ react-blog-app/src/pages/home/index.tsx
@@ -1,36 +1,13 @@
 import {Link} from "react-router-dom";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import PostList from "../../components/PostList";
 
 export default function Home() {
     return (
         <div>
             <Header />
-            <div className="post__navigation">
-                <div className="post__navigation--active">전체</div>
-                <div>나의 글</div>
-            </div>
-            <div className="post__list">
-                {[...Array(10)].map((e, index) => (
-                    <div key={index} className="post__box">
-                        <Link to={`/posts/${index}`}>
-                            <div className="post__profile-box">
-                                <div className="post__profile" />
-                                <div className="post__author-name">작성자명</div>
-                                <div className="post__date">2024.10.15 화요일</div>
-                            </div>
-                            <div className="post__title">게시글 {index}</div>
-                            <div className="post__text">
-                                내용내용
-                            </div>
-                            <div className="post__utils-box">
-                                <div className="post__delete">삭제</div>
-                                <div className="post__edit">수정</div>
-                            </div>
-                        </Link>
-                    </div>
-                ))}
-            </div>
+            <PostList />
             <Footer />
         </div>
     );
--- react-blog-app/src/pages/profile/index.tsx
+++ react-blog-app/src/pages/profile/index.tsx
@@ -1,3 +1,15 @@
-export default function Profile() {
-    return <h1>PROFILE</h1>;
+import Profile from "../../components/Profile";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import PostList from "../../components/PostList";
+
+export default function ProfilePage() {
+    return (
+        <>
+            <Header />
+            <Profile />
+            <PostList hasNavigation={false} />
+            <Footer />
+        </>
+    );
 }
