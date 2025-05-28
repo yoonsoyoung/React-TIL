ac57331e0a2c80b7b6781c1ca32d0fe58c9bb3d6
yoonsoyoung <hummingsy@gmail.com>
2025-05-28 22:55:58 +0900

    Feat feature/2-3: Update 글쓰기, 수정 폼 컴포넌트 추가+css작업

--- react-blog-app/src/components/PostDetail.tsx
+++ react-blog-app/src/components/PostDetail.tsx
@@ -1,3 +1,5 @@
+import { Link} from "react-router-dom";
+
 export default function PostDetail() {
     return <>
         <div className="post__detail">
@@ -10,7 +12,9 @@ export default function PostDetail() {
                 </div>
                 <div className="post__utils-box">
                     <div className="post__delete">삭제</div>
-                    <div className="post__edit">수정</div>
+                    <div className="post__edit">
+                        <Link to={`/posts/edit/1`} >수정</Link>
+                    </div>
                 </div>
                 <div className="post__text">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
