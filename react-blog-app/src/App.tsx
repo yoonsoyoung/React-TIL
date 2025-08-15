01a4d8617c97fc79459ec5065218bc3f34f1d8df
yoonsoyoung <hummingsy@gmail.com>
2025-08-15 23:07:03 +0900

    Feat feature/2-4: Update firebase 회원가입 구현 및 성공유무 toast 추가

--- react-blog-app/src/App.tsx
+++ react-blog-app/src/App.tsx
@@ -1,12 +1,19 @@
 import { useState} from "react";
 import {app} from "./firebaseApp";
 import { getAuth } from "firebase/auth";
+import { ToastContainer, toast } from 'react-toastify';
+
 import Router from "./components/Router";
 
 function App() {
     const auth = getAuth(app);
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
-    return <Router isAuthenticated={isAuthenticated}/>;
+    return (
+        <>
+            <ToastContainer />
+            <Router isAuthenticated={isAuthenticated}/>
+        </>
+    );
 }
 
 export default App;
