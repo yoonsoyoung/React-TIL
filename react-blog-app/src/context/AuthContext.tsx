3082db46d033490c42d23558a4373cddcb550550
yoonsoyoung <hummingsy@gmail.com>
2025-08-19 22:48:53 +0900

    Feat feature/2-4: Update Context API를 사용하여 사용자 정보 관리 개선

---
+++ react-blog-app/src/context/AuthContext.tsx
@@ -0,0 +1,34 @@
+import {createContext, ReactNode, useEffect, useState} from "react";
+import {getAuth, onAuthStateChanged, User} from 'firebase/auth';
+import {app} from "../firebaseApp";
+
+interface AuthProps {
+    children: ReactNode;
+}
+
+const AuthContext = createContext({
+    user: null as User | null,
+});
+
+export const AuthContextProvider = ({ children }: AuthProps) => {
+    const auth = getAuth(app);
+    const [ currentUser, setCurrentUser ] = useState<User | null>(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user) {
+                setCurrentUser(user);
+            } else {
+                setCurrentUser(user);
+            }
+        })
+    }, [auth]); // 처음에만 변경됨
+
+    return (
+        <AuthContext.Provider value={{user: currentUser}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
