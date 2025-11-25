2f784e180ae9fc7c2e6442801e699c766786b02b
yoonsoyoung <hummingsy@gmail.com>
2025-11-26 00:49:22 +0900

    Desgin feature/2-7: Update 다크모드 CSS 추가

--- react-blog-app/src/App.tsx
+++ react-blog-app/src/App.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState, useContext} from "react";
 import { app } from "./firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer, toast } from 'react-toastify';
+import ThemeContext from "./context/ThemeContext";
 
 import Router from "./components/Router";
 import Loader from "./components/Loader";
 
 function App() {
+    const context = useContext(ThemeContext);
     const auth = getAuth(app);
 
     // auth를 체크하기 전(initialize 전)에는 loader를 띄워주는 용도
@@ -26,10 +28,10 @@ function App() {
        })
     }, [auth]); // 처음에만 변경됨
     return (
-        <>
+        <div className={context.theme === 'light' ? 'white' : 'dark'}>
             <ToastContainer />
             {init ? <Router isAuthenticated={isAuthenticated}/> : <Loader />}
-        </>
+        </div>
     );
 }
 
