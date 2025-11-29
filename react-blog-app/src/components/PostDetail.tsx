310cfe744144bf90ceab6f76f68918853725e19a
yoonsoyoung <hummingsy@gmail.com>
2025-11-29 22:37:42 +0900

    Feat feature/2-8: Update 게시글 상세 수정,삭제 사용자 권한 체크 추가

--- react-blog-app/src/components/PostDetail.tsx
+++ react-blog-app/src/components/PostDetail.tsx
@@ -1,15 +1,17 @@
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import {useState, useEffect, useContext} from "react";
 import {PostProps} from "./PostList";
 import { doc, getDoc, deleteDoc } from "firebase/firestore";
 import { db } from "firebaseApp";
 import Loader from "./Loader";
 import {toast} from "react-toastify";
+import AuthContext from "../context/AuthContext";
 
 export default function PostDetail() {
     const [post, setPost] = useState<PostProps | null>(null);
     const params = useParams();
     const navigate = useNavigate();
+    const {user} = useContext(AuthContext);
 
     const getPost = async (id: string) => {
         if(id) {
@@ -44,13 +46,15 @@ export default function PostDetail() {
                         <div className="post__date">{post?.createAt}</div>
                     </div>
                     <div className="post__utils-box">
-                        {post?.category && (
-                            <div className="post__category">{post?.category}</div>
+                        <div className="post__category">{post?.category || "자유주제"}</div>
+                        {post?.uid === user?.uid && (
+                            <>
+                                <div className="post__delete" onClick={handleDelete}>삭제</div>
+                                <div className="post__edit">
+                                    <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                                </div>
+                            </>
                         )}
-                        <div className="post__delete" onClick={handleDelete} >삭제</div>
-                        <div className="post__edit">
-                            <Link to={`/posts/edit/${post?.id}`} >수정</Link>
-                        </div>
                     </div>
                     <div className="post__text post__text--pre-wrap">
                         {post?.content}
